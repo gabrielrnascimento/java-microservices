287b767396b098141dd912be1b72d4a6d7ee81ff
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-09 23:55:50 -0300

    feat(user): send email after saving user on database

--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.EmailProducer;
 import com.ms.user.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,13 +10,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     final UserRepository userRepository;
+    final EmailProducer emailProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, EmailProducer emailProducer) {
         this.userRepository = userRepository;
+        this.emailProducer = emailProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel) {
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        emailProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
